3439a96ccd262452c0ec77e96bcd63600adce684
rsraselkhan <164967114+rsraselkhan@users.noreply.github.com>
2024-04-18 08:51:24 +0600

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
+API_ID       = 22981998
+API_HASH     = "ee0e5a596e2e61cac1fe1dbe793740cc"
+BOT_TOKEN    = "6736052548:AAEMWHFN9MUu4isC3l7l7UX49NAXuadSGFs"
 SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+DATABASE_URI = "mongodb+srv://emotion0852:jCzALlSIFvHAgRKe@cluster0.o8jheuu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+LOG_CHANNEL  = -1002076387294
+ADMIN        = 6578386545
+CHANNEL      = "@RS_Botz"
